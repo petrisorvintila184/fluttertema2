818d2ba27abfaa9afbc23ae93c4451c0ac14aeae
VintilaPetrisor <99169002+VintilaPetrisor@users.noreply.github.com>
2023-04-10 20:06:40 +0300

    Curs 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,15 @@
-
-import 'package:flutter/material.dart';
 import 'dart:math';
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(const CursFlutterTema2());
 }
 
 class CursFlutterTema2 extends StatelessWidget {
-  const CursFlutterTema2({Key? key}) : super(key: key);
+  const CursFlutterTema2({super.key});
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return const MaterialApp(
       home: CursFlutterTema2Home(),
     );
@@ -18,26 +17,26 @@ class CursFlutterTema2 extends StatelessWidget {
 }
 
 class CursFlutterTema2Home extends StatefulWidget {
-  const CursFlutterTema2Home({Key? key}) : super(key: key);
+  const CursFlutterTema2Home({super.key});
 
   @override
   State<CursFlutterTema2Home> createState() => CursFlutterTema2HomeState();
 }
 
-class CursFlutterTema2HomeState extends State<CursFlutterTema2Home>{
-  final numberController = TextEditingController();
+class CursFlutterTema2HomeState extends State<CursFlutterTema2Home> {
+  final TextEditingController numberController = TextEditingController();
   bool isSquare = false;
   bool isTriangle = false;
 
   void checkNumber() {
-    int number = int.tryParse(numberController.text) ?? 0;
-    int squareRoot = sqrt(number).toInt();
+    final int number = int.tryParse(numberController.text) ?? 0;
+    final int squareRoot = sqrt(number).toInt();
     int z = 0;
     String val = 'enter a number';
 
     setState(() {
       if (number != 0) {
-        isSquare = (squareRoot * squareRoot == number);
+        isSquare = (squareRoot * squareRoot) == number;
 
         for (int i = 1; i < number; i++) {
           if ((i * i * i) == number) {
@@ -55,85 +54,80 @@ class CursFlutterTema2HomeState extends State<CursFlutterTema2Home>{
 
         if (isTriangle) {
           if (isSquare) {
-            val = "Number is square and triangle.";
+            val = 'Number is square and triangle.';
           } else {
-            val = "Number is triangle.";
+            val = 'Number is triangle.';
           }
         } else {
           if (isSquare) {
-            val = "Number is square.";
+            val = 'Number is square.';
           } else {
             val = 'Number is no square or triangle';
           }
         }
-      }else{
+      } else {
         val = 'Insert a correct number!';
       }
-    }
-      );
+    });
 
     showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text('Number Checker'),
-          content: Text(
-            'The number ${numberController.text} is: ${val}'
-          ),
-
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Number Checker'),
+            content: Text('The number ${numberController.text} is: $val'),
           );
-      }
-    );
+        });
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold(
-      appBar: AppBar(
-        title: const Text('Number Shapes'),
-        centerTitle: true,
-      ),
-      body: Column(
-        children: <Widget>[
-          const Text('Please enter a number to see if it is square or triangular.',
-            style:TextStyle(
-              fontSize: 25,
-            ),
-          ),
-
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-            child: TextFormField(
-              controller: numberController,
-              decoration: const InputDecoration(
-                border: UnderlineInputBorder(),
-                labelText: 'Enter your number',
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Number Shapes'),
+          centerTitle: true,
+        ),
+        body: Column(
+          children: <Widget>[
+            const Text(
+              'Please enter a number to see if it is square or triangular.',
+              style: TextStyle(
+                fontSize: 25,
               ),
             ),
-          ),
-        ],
-      ),
-      bottomNavigationBar: Container(
-          padding: const EdgeInsets.all(2),
-          height: 70,
-          margin: const EdgeInsets.only(left: 20.0, right: 40.0, bottom: 40.0),
-          color: Colors.white,
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            textDirection: TextDirection.rtl,
-            children: [
-              IconButton(
-                icon: const Icon(
-                  Icons.check_circle_rounded,
-                  color:Colors.blue,
-                  size: 70,
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+              child: TextFormField(
+                controller: numberController,
+                decoration: const InputDecoration(
+                  border: UnderlineInputBorder(),
+                  labelText: 'Enter your number',
                 ),
-                onPressed: checkNumber,
               ),
-            ],
-          )
+            ),
+          ],
+        ),
+        bottomNavigationBar: Container(
+            padding: const EdgeInsets.all(2),
+            height: 70,
+            margin: const EdgeInsets.only(left: 20.0, right: 40.0, bottom: 40.0),
+            color: Colors.white,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              textDirection: TextDirection.rtl,
+              children: <Widget>[
+                IconButton(
+                  icon: const Icon(
+                    Icons.check_circle_rounded,
+                    color: Colors.blue,
+                    size: 70,
+                  ),
+                  onPressed: checkNumber,
+                ),
+              ],
+            )),
       ),
-    ),
     );
   }
 }
